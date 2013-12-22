ad7aab0004b2b307312c81a5ce61286af984676d
cyanmage <cyanmagespirit@gmail.com>
2013-12-22 22:06:28 +0100

    Sauvegarde de la version avec gestion des dates
    Dans les prochaines versions, la gestion des dates sera dans un module différent.

--- js/interactivite_avec_angular.js
+++ js/interactivite_avec_angular.js
@@ -1,4 +1,4 @@
-var appli = angular.module('mon_appli', []);
+var appli = angular.module('appli', ['defileur']);
 
 
 appli.config(['$interpolateProvider', function($interpolateProvider) {
@@ -6,84 +6,184 @@ appli.config(['$interpolateProvider', function($interpolateProvider) {
   	$interpolateProvider.endSymbol('*}');    
        }
 ]);
+var module_defileur = angular.module('defileur', []);
 
-appli.factory('serviceDates', ['$q', function($q){
+
+
+
+
+/*/////////////////////////////////////////////////////////////////////////////////*/
+///////////////////////////////////////////////////////////////////////////////////*/
+/*                                                                                 */
+/*                                                                                 */
+/*                    Controleur associé au défileur                               */
+/*                                                                                 */
+/*                                                                                 */
+///////////////////////////////////////////////////////////////////////////////////*/
+/////////////////////////////////////////////////////////////////////////////////////
+
+
+module_defileur.controller("controleur_defileur", ['$q', 'gestionDesPanneaux', '$scope', function($q, gestionDesPanneaux, $scope){
+
+ 	$scope.libelleBtnAttachDetach = "Détacher";
+ 	$scope.gestionDesPanneaux = gestionDesPanneaux;
+
+	$scope.$on("changementDirection", function(event, data){
+		console.log(data);
+	});
+
+
+	$scope.cache = new Array();
+
+	$scope.attacheDetacheStickers= function(){
+		if ($scope.libelleBtnAttachDetach ==  "Attacher"){
+			$scope.libelleBtnAttachDetach =  "Détacher"; 
+			$scope.cache.push($(".sticker").detach());
+		}
+		else{
+			$scope.libelleBtnAttachDetach =  "Attacher"; 	
+		}	 
+	}
+
+}]);
+
+
+
+/*#############################             ###############################          ################################*/
+/*#############################             ###############################          ################################*/
+/*#############################             ###############################          ################################*/
+
+//													  DIRECTIVES
+
+/*#############################             ###############################          ################################*/
+/*#############################             ###############################          ################################*/
+/*#############################             ###############################          ################################*/
+
+
+module_defileur.directive('defileur', ['$q','gestionDesPanneaux', function($q, panneaux){
+ 	return {
+ 		restrict: 'A',
+ 		link : function(scope, element, attributes){
+ 			//element.find(".centerPanel").addClass()
+ 			//panneaux.initialisePanneaux();	
+ 		}
+	}
+}]);
+
+
+module_defileur.directive('directionDroite', ['gestionDesPanneaux', function(panneaux){
+
+	return {
+		restrict : 'A',
+		link : function(scope, element, attributes) {
+			element.on("click", function(event){
+				panneaux.defilementVersLaDroite(scope);
+				scope.$emit("changementDirection", "flecheGauche");
+			});	
+		}	
+
+	}
+
+}]);
+
+module_defileur.directive('directionGauche', ['gestionDesPanneaux', function(panneaux){
+
+	return {
+		restrict : 'A',
+		link : function(scope, element, attributes) {
+			element.on("click", function(event){
+				panneaux.defilementVersLaGauche(scope);
+				scope.$emit("changementDirection", "flecheDroite");				
+			});	
+		}	
+
+	}
+
+}]);
+
+
+
+
+
+
+/******************************************************************************/
+/******************************************************************************/
+/******************************************************************************/
+/******************            LES SERVICES         ***************************/
+/******************************************************************************/
+/******************************************************************************/
+/******************************************************************************/
+/******************************************************************************/
+
+module_defileur.factory('serviceDates', ['$q', function($q){
 	var les_services_dates = {};
+
 
 	var tableau_des_dates = les_services_dates.tableau_des_dates = new Array();
 
-	function calculeDates(position, debutSemaine){
+	var nbJoursDecalage = 1;
+
+	var calculeDates = function(position, debutSemaine){
 		var finSemaine = new Date(debutSemaine);
 		finSemaine.setDate(debutSemaine.getDate() + 6) ;
 		tableau_des_dates[position] = [debutSemaine, finSemaine];
-		//console.log(tableau_des_dates);
 	}
+
+	/*Retourne une date formatée sous la forme yyyymmjj*/
+	les_services_dates.retourneDateEnCours = function(date){
+		var date = tableau_des_dates[1][0];
+		return "" + date.getFullYear() + (date.getMonth() + 1) + date.getDate();
+ 	}
 
 	les_services_dates.initialiseBufferDates = function(){
 		var dateAvant = new Date(), dateMaintenant = new Date(), dateApres = new Date();
-		var day = dateMaintenant.getDay();
-		diff = dateMaintenant.getDate() - day + (day == 0 ? -6:1); 
-		//console.log(dateMaintenant);
-		calculeDates(1, new Date(dateMaintenant.setDate(diff)));
-		calculeDates(0, new Date(dateAvant.setDate(diff - 7)));		
-		calculeDates(2, new Date(dateApres.setDate(diff + 7)));
-	}
- 	les_services_dates.metAJourBufferDates = function(decalage){
- 		//console.log(decalage);
-		if (decalage == "shiftRight"){
+		diff = 0;
 
+		var day = dateMaintenant.getDay();
+		//diff = dateMaintenant.getDate() - day + (day == 0 ? -6:1); Utilisé pour semaine à semaine
+		diff = dateMaintenant.getDate()
+
+		calculeDates(0, new Date(dateAvant.setDate(diff - nbJoursDecalage)));			
+		calculeDates(1, new Date(dateMaintenant.setDate(diff)));
+		calculeDates(2, new Date(dateApres.setDate(diff + nbJoursDecalage)));
+	}
+
+ 	les_services_dates.metAJourBufferDates = function(decalage){
+		if (decalage == "shiftRight"){
 			angular.copy(tableau_des_dates[1],tableau_des_dates[0]) ;
 			angular.copy(tableau_des_dates[2],tableau_des_dates[1]) ;			
-			//tableau_des_dates[1] = tableau_des_dates[2];
 			var d = new Date(tableau_des_dates[2][0]);
-			calculeDates(2, new Date(d.setDate(d.getDate() + 7)));	
-		//console.log(tableau_des_dates);
+			calculeDates(2, new Date(d.setDate(d.getDate() + nbJoursDecalage)));	
 		}
 		else 
 		if (decalage == "shiftLeft"){
-			
-			/*tableau_des_dates[2] = tableau_des_dates[1];
-			tableau_des_dates[1] = tableau_des_dates[0];*/
 			angular.copy(tableau_des_dates[1],tableau_des_dates[2]) ;
 			angular.copy(tableau_des_dates[0],tableau_des_dates[1]) ;				
 			var d = new Date(tableau_des_dates[0][0]);
-			calculeDates(0, new Date(d.setDate(d.getDate() - 7)));	
+			calculeDates(0, new Date(d.setDate(d.getDate() - nbJoursDecalage)));	
 
 		}
-		//console.log(tableau_des_dates);		
+	
 }
 	return les_services_dates;
 }]);
 
 
 
-appli.factory('gestionDesPanneaux',[ 'serviceDates', '$q', function(serviceDates, $q){
+
+
+
+module_defileur.factory('gestionDesPanneaux',['$q', function($q){
 var gestion_des_panneaux = {};
 
 	var animation_en_cours = false;	
 	var t_rightShift  = [1, 2, 0];
 	var t_leftShift   = [2, 0, 1];
 
+
+	//Publiques, car doivent être accessibles de l'extérieur par angularjs//
 	gestion_des_panneaux.champ_gauche = 0, gestion_des_panneaux.champ_centre = 1, gestion_des_panneaux.champ_droite = 2;
-	
 
-	/*function metAJourPanneau(panneau, numero_panneau){
-		var debutSemaine = serviceDates.tableau_des_dates[numero_panneau][0], 
-			finSemaine   = serviceDates.tableau_des_dates[numero_panneau][1];
-
-		texte_date = "Semaine du " + debutSemaine.getDate() + "/" + (debutSemaine.getMonth() + 1)  +  "/"  +  debutSemaine.getFullYear();
-		texte_date +=  " au " 	   + finSemaine.getDate()   + "/" + (finSemaine.getMonth() + 1)    +  "/"  +  finSemaine.getFullYear();
-		champDate			= $("." + panneau).find(".champDate");
-		champDate.text(texte_date);	
-	}*/
-
-	gestion_des_panneaux.initialisePanneaux = function(){
-		serviceDates.initialiseBufferDates();
-		//console.log(serviceDates.tableau_des_dates);
-		/*gestion_des_panneaux.metAJourPanneau("leftPanel"   , 0);
-		gestion_des_panneaux.metAJourPanneau("centerPanel" , 1);
-		gestion_des_panneaux.metAJourPanneau("rightPanel"  , 2);*/
-	}
 
 
 	gestion_des_panneaux.defilementVersLaDroite = function(scope){
@@ -101,39 +201,13 @@ var gestion_des_panneaux = {};
 									"slow"
 						)
 	 			.queue(function(){
-					serviceDates.metAJourBufferDates("shiftRight");
-					console.log("vers la droite");
-					//metAJourPanneau("leftPanel", 2);
-					//console.log(t_leftShift[gestion_des_panneaux.champ_gauche]);					
+					//serviceDates.metAJourBufferDates("shiftRight");
+		
 					gestion_des_panneaux.champ_gauche = t_leftShift[gestion_des_panneaux.champ_gauche];
-					//console.log(gestion_des_panneaux.champ_gauche);	
-
-
-					//console.log(t_leftShift[gestion_des_panneaux.champ_centre]);	
 					gestion_des_panneaux.champ_centre = t_leftShift[gestion_des_panneaux.champ_centre];
-					//console.log(gestion_des_panneaux.champ_centre);		
-									
-					//console.log(t_leftShift[gestion_des_panneaux.champ_droite]);				
 					gestion_des_panneaux.champ_droite = t_leftShift[gestion_des_panneaux.champ_droite];
-					//console.log(gestion_des_panneaux.champ_droite);							
-
-
-					//console.log(gestion_des_panneaux.champ_gauche  + "-" + gestion_des_panneaux.champ_centre + "-" + gestion_des_panneaux.champ_droite);
-					// angular.copy(t_leftShift[gestion_des_panneaux.champ_gauche], gestion_des_panneaux.champ_gauche);
-					// angular.copy(t_leftShift[gestion_des_panneaux.champ_droite], gestion_des_panneaux.champ_droite);
-					// angular.copy(t_leftShift[gestion_des_panneaux.champ_centre], gestion_des_panneaux.champ_centre);					
-					/*texte_date = "Semaine du " + debutSemaine.getDate() + "/" + (debutSemaine.getMonth() + 1)  +  "/"  +  debutSemaine.getFullYear();
-					texte_date +=  " au " 	   + finSemaine.getDate()   + "/" + (finSemaine.getMonth() + 1)					
-					champDate			= $(".leftPanel").find(".champDate").text(texte_date);	*/				
-					scope.$apply();
 			
-					//console.log(gestion_des_panneaux.champ_gauche  + "-" + gestion_des_panneaux.champ_centre + "-" + gestion_des_panneaux.champ_droite);
-					/*console.log("scope");
-					console.log(scope.champ_gauche);		
-					console.log(scope.champ_centre);			
-					console.log(scope.champ_droite);
-					console.log(scope.dates_a_afficher);	*/		
-					//console.log(serviceDates.tableau_des_dates[0]);
+					scope.$apply();
 					$(this).dequeue();	 				
 	 			})				
 				.queue(function(){
@@ -146,13 +220,14 @@ var gestion_des_panneaux = {};
 					droite.removeClass("rightPanel").addClass("centerPanel");
 					gauche.removeClass("leftPanel").addClass("rightPanel");
 					centre.addClass("leftPanel");
+
+
 	
 					animation_en_cours = false ;				
 					$(this).dequeue();
 				});
 			}
 	}		
-
 
 	gestion_des_panneaux.defilementVersLaGauche = function(scope){
 
@@ -168,17 +243,12 @@ var gestion_des_panneaux = {};
 	 							"slow"
 	 				)
 	 			.queue(function(){
-					serviceDates.metAJourBufferDates("shiftLeft");
-					//metAJourPanneau("rightPanel", 0);					
-					 /*angular.copy(t_rightShift[gestion_des_panneaux.champ_gauche], gestion_des_panneaux.champ_gauche);
-					 angular.copy(t_rightShift[gestion_des_panneaux.champ_droite], gestion_des_panneaux.champ_droite);
-					 angular.copy(t_rightShift[gestion_des_panneaux.champ_centre], gestion_des_panneaux.champ_centre);	*/
+					//serviceDates.metAJourBufferDates("shiftLeft");
+
 					gestion_des_panneaux.champ_gauche = t_rightShift[gestion_des_panneaux.champ_gauche];
 					gestion_des_panneaux.champ_centre = t_rightShift[gestion_des_panneaux.champ_centre];
 					gestion_des_panneaux.champ_droite = t_rightShift[gestion_des_panneaux.champ_droite];					 				
-					/*texte_date = "Semaine du " + debutSemaine.getDate() + "/" + (debutSemaine.getMonth() + 1)  +  "/"  +  debutSemaine.getFullYear();
-					texte_date +=  " au " 	   + finSemaine.getDate()   + "/" + (finSemaine.getMonth() + 1)					
-					champDate			= $(".rightPanel").find(".champDate").text(texte_date);	*/				
+		
 					scope.$apply();
 					$(this).dequeue();	 				
 	 			})
@@ -188,10 +258,11 @@ var gestion_des_panneaux = {};
 					var centre = $(".centerPanel"), droite = $(".rightPanel"), gauche = $(".leftPanel");	
 		
 					centre.removeClass("centerPanel");
+					//centre.removeClass("panneauActif");
 					droite.removeClass("rightPanel").addClass("leftPanel");
 					gauche.removeClass("leftPanel").addClass("centerPanel");
+					//gauche.addClass("panneauActif");						
 					centre.addClass("rightPanel");
-					//console.log()
 
 					animation_en_cours = false ;		
 					$(this).dequeue();
@@ -202,77 +273,6 @@ var gestion_des_panneaux = {};
 	return gestion_des_panneaux;
 }]);
 
-
-
-
-
-appli.directive('defileur', ['$q','gestionDesPanneaux', function($q, panneaux){
- 	return {
- 		restrict: 'A',
- 		link : function(scope, element, attributes){
- 			panneaux.initialisePanneaux();	
- 		}
-	}
-}]);
-
-
-
-
-appli.directive('directionDroite', ['gestionDesPanneaux', 'serviceDates', function(panneaux, serviceDates){
-
-	return {
-		restrict : 'A',
-		link : function(scope, element, attributes) {
-			element.on("click", function(event){
-				panneaux.defilementVersLaDroite(scope);
-			});	
-		}	
-
-	}
-
-}]);
-
-appli.directive('directionGauche', ['gestionDesPanneaux', function(panneaux){
-
-	return {
-		restrict : 'A',
-		link : function(scope, element, attributes) {
-			element.on("click", function(event){
-				panneaux.defilementVersLaGauche(scope);
-			});	
-		}	
-
-	}
-
-}]);
-
-
-appli.controller("controleur", ['$q', 'serviceDates', 'gestionDesPanneaux', '$scope', function($q, serviceDates, gestionDesPanneaux, $scope){
-
- 	$scope.dates_a_afficher = serviceDates.tableau_des_dates;
- 	$scope.gestionDesPanneaux = gestionDesPanneaux;
- 	//$scope.champ_gauche = gestionDesPanneaux.champ_gauche/**/;
- 	//$scope.champ_centre = gestionDesPanneaux.champ_centre/**/;
-	//$scope.champ_droite = gestionDesPanneaux.champ_droite/**/;
-
- 	/*$scope.$watch('dates_a_afficher', function(newValue, oldValue) {
- 		if (newValue !== oldValue){
- 			console.log(newValue);
- 		}
-
- 	}, true);*/
-
-
-$scope.$watch('gestionDesPanneaux.champ_gauche', function(newValue, oldValue) {
- 		//if (newValue !== oldValue){
- 			console.log("champ gauche change : " + newValue);
- 		//}
-
- 	}, true);
-
-
-
-}]);
 
 
 
